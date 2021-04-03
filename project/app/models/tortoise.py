96c47ed8b13777bc3263ebc629a0fabee013b99f
Ruben Castro Espinoza <rubencastroe@gmail.com>
2021-04-03 11:56:45 -0700

    configured and connected to postgress db successfully

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -4,7 +4,7 @@ from tortoise import fields, models
 class TextSummary(models.Model):
     url = fields.TextField()
     summary = fields.TextField()
-    create_at = fields.DatetimeField(auto_now_add=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
 
     def __str__(self):
         return self.url
